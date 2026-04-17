f5a32ab98b0f9af720fd7d5f41bbcc8c13162337
spencjo1 <spencjo@umich.edu>
2026-04-16 20:20:44 -0400

    Delete src directory

--- src/FoodRating.java
+++
@@ -1,9 +0,0 @@
-public class FoodRating {
-    public String user_name;
-    public int user_id;
-    public int rating;
-    public String food_name;
-    public double distance;
-    public int open_time;
-    public int close_time;
-}
